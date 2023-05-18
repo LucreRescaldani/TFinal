41bf46c711b385c78b5c14f57be227ded8380c94
Lucrecia Rescaldani <131829959+LucreRescaldani@users.noreply.github.com>
2023-05-17 23:45:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var nombre=document.getElementById('nombreInput');
+var apellido=document.getElementById('apellidoInput');
+var email=document.getElementById('emailInput');
+var cantidadTicket=document.getElementById('cantidadInput');
+var tipoTicket=document.getElementById('categoriaInput');
+
+alert("Hola!!")
+
+console.log("Nombre: " + nombre);
+console.log("Apellido: " + apellido);
+console.log("Correo: " + email);
+console.log("Cantidad: " + cantidadTicket);
+console.log("Categoria: " + tipoTicket);
+
+
+if (cantidadTicket==1) {
+    if (tipoTicket==3){
+        console.log("Total a Pagar $ 200");
+    }
+}
